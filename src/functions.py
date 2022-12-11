f7ddf876acc5bc4fb40dcd34f3eef1dc462d9c15
ck9 <0926cookie@gmail.com>
2022-12-11 21:18:43 +0900

    add getSignFromPoC()

---
+++ src/functions.py
@@ -0,0 +1,21 @@
+import os, re
+
+def getSignFromPoC(codePath):
+  pathList = []
+  if not os.path.isfile(codePath):
+    return pathList
+  with open(codePath, 'r') as f:
+    code = f.read()
+    pattern = r'[\'"](\{.+?\})*((\/[\w:%#\$&\?\(\)~\.=\+\-]+){2,})(\{.+?\})*[\'"]'
+    for line in code.splitlines():
+      match = re.search(pattern, line)
+      if match:
+        pathList.append(match.group(2))
+    if not pathList:
+      pattern = r'^([A-Z]+)\s((\/[\w:%#\$&\?\(\)~\.=\+\-]+)+)\sHTTP'
+      for line in code.splitlines():
+        match = re.search(pattern, line)
+        if match:
+          pathList.append(match.group(2))
+  return pathList
+  
