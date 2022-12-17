d4e4a30de7a9989cca3956b163191edbcb0384f6
ck9 <0926cookie@gmail.com>
2022-12-17 23:25:04 +0900

    add update_github()

--- src/github/main.py
+++ src/github/main.py
@@ -1,6 +1,12 @@
-import os, glob, json
+import os, glob, json, sys
+from datetime import datetime
+from dotenv import load_dotenv
+from progressbar import ProgressBar
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+from functions import *
 
 thisDir = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(thisDir, '../.env'))
 
 def update_github():
   if not os.path.isdir(os.path.join(thisDir, 'PoC-in-GitHub')):
@@ -8,10 +14,16 @@ def update_github():
     os.system(
         "cd %s; git clone https://github.com/nomi-sec/PoC-in-GitHub.git" % thisDir
     )
+  else:
+    print("Updating exploit-database repository")
+    os.system(
+        "cd %s; cd PoC-in-GitHub; git pull" % thisDir
+    )
   PoCdata = {}
   yearList = glob.glob(os.path.join(thisDir, 'PoC-in-GitHub/*'))
   yearList = [os.path.basename(year) for year in yearList]
   yearList = [year for year in yearList if year.isdigit()]
+  pLen = 0
   for year in yearList:
     CVEList = glob.glob(os.path.join(thisDir, 'PoC-in-GitHub/%s/*' % year))
     for CVEjsonPath in CVEList:
@@ -24,9 +36,35 @@ def update_github():
             "full_name": data['full_name'],
             "url": data['html_url'],
             "description": data['description'],
-            "created_at": data['created_at'],
+            "created_at": datetime.strptime(data['created_at'], '%Y-%m-%dT%H:%M:%SZ').strftime('%Y-%m-%d %H:%M:%S'),
           })
-
+          pLen += 1
   with open(os.path.join(thisDir, 'github_PoC.json'), 'w') as f:
     json.dump(PoCdata, f, indent=2)
+  
+  # insert new PoC data to DB
+  db_conn = connectPoCDB()
+  PoCurlList = getPoCurlList(db_conn)
+  print("Importing to Database...")
+  pCnt = 0
+  with ProgressBar(max_value=pLen) as pbar:
+    for cve in PoCdata.keys():
+      for data in PoCdata[cve]:
+        pCnt += 1
+        pbar.update(pCnt)
+        if data['url'] in PoCurlList:
+          continue
+        PoCurlList.append(data['url'])
+        PoCinfo = {
+          "cve": cve,
+          "url": data['url'],
+          "path": "None",
+          "description": data['description'],
+          "created_at": data['created_at'],
+          "signatures": [],
+        }
+        insertPoCDB(PoCinfo, db_conn)
+  closePoCDB(db_conn)
 
+
+update_github()
